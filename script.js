1ba1ebdf5dd52525c92dd028e9d50d30e610f959
zMag33z <mag33.cdh@gmail.com>
2022-11-27 13:23:34 -0600

    Need to finish README

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
-  /* Work Day Scheduler.*/
+/* Work Day Scheduler.*/
 
+/* Allow document to load first.*/
 $(document).ready(function(){
-
-    /* Each button is tied to its own event to be saved to localstorage. */
-
+  
+/* Each button is tied to its own event to be saved to localstorage. */
   $('.saveBtn').each(function(){ 
   $(this).on('click', function(){
-    var eventTime = $(this).parent().attr('id');  // this button's parents attribute id name = ex:  9AM, 10AM, 11AM;  Used as key name.
-    var eventTxt = $(this).prev().val();
+    let eventTime = $(this).parent().attr('id');  // Each button's parent's attribute id name = ex:  '09', '10', '11';  Used as key name.
+    let eventTxt = $(this).prev().val();    //Grab each child's value that is before the button.
     localStorage.setItem(eventTime, eventTxt);
     })
   })
 
-    /* Checks each areas id to the time to change background to/from past present future. */    
-
-  $('.hour').each(function(){
-    var rowTime = $(this).parent().attr('id');    
-    var currentEvent = dayjs().format('HH');
-    if(rowTime > currentEvent){
-      $(this).parent().removeClass('past').addClass('future');
-    }else if(rowTime < currentEvent){
-      $(this).parent().removeClass('present').addClass('past');
-    }else if(rowTime === currentEvent){
-      $(this).parent().removeClass('future').addClass('present');
-    }
-  })
-
-    /* Pulls saved events from Localstorage to continually be displayed in text area */
-
+/* Pulls saved events from Localstorage.
+   They are respective to their own parent.*/
   $('.description').each(function(){
     $(this).val(localStorage.getItem($(this).parent().attr('id')));
   })
 
-/* Displays current Day, Date, Time. */
+/* Displays current Day, Date, Time to the second.
+   Continuously checks current time to add class past/present/future.*/
+  setInterval(function(){
 
-  function currentTime(){
-    var reformatDate = dayjs().format('ddd, MMMM D YYYY, h:mm:ss a');
-    $('#currentDay').text(reformatDate);
-  }
-  setInterval(currentTime, 1000);
+    let reformatDate = dayjs().format('ddd, MMMM D YYYY, h:mm:ss a');
+    $('#currentDay').text(reformatDate); //display current time to the second.
+
+    $('.hour').each(function(){   //checks parents id to the current hour.
+      let rowTime = $(this).parent().attr('id');    
+      let currentEvent = dayjs().format('HH');
+      if(rowTime > currentEvent){   //depending on eachother's value from one another change class.
+        $(this).parent().removeClass('past').addClass('future');
+      }else if(rowTime < currentEvent){
+        $(this).parent().removeClass('present').addClass('past');
+      }else if(rowTime === currentEvent){
+        $(this).parent().removeClass('future').addClass('present');
+      }
+    })
+  }, 1000);
+
 });
+
+
+
+/*  zMaG33z  */
